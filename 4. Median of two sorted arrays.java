1bf1da558169f20cf22c700a0257f1720bc2f1e2
sgupta <sgupta99299@gmail.com>
2024-03-22 14:25:37 +0530

    Leet Code Solution

---
+++ 4. Median of two sorted arrays.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=4 lang=java
+ *
+ * [4] Median of Two Sorted Arrays
+ */
+
+// @lc code=start
+import java.util.Arrays;
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int[] merged = new int[n + m];
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            merged[k++] = nums1[i];
+        }
+        for (int i = 0; i < m; i++) {
+            merged[k++] = nums2[i];
+        }
+        Arrays.sort(merged);
+        int total = merged.length;
+
+        if (total % 2 == 1) {
+            return (double) merged[total / 2];
+        } else {
+            int middle1 = merged[total / 2 - 1];
+            int middle2 = merged[total / 2];
+            return ((double) middle1 + (double) middle2) / 2.0;
+        }
+    }
+}
+// @lc code=end
+
